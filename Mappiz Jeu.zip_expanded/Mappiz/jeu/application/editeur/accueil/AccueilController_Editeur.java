8a8b20d8ce5a9e2e5e367ca0342468eeb572f7cd
Arthur Cheramy <arthur.cheramy@gmail.com>
2019-02-15 14:51:09 -0500

    Fix typo

--- Mappiz Jeu.zip_expanded/Mappiz/jeu/application/editeur/accueil/AccueilController_Editeur.java
+++ Mappiz Jeu.zip_expanded/Mappiz/jeu/application/editeur/accueil/AccueilController_Editeur.java
@@ -308,6 +308,7 @@ public class AccueilController_Editeur {
 		Platform.exit();
 		System.exit(0);
 	}
+	
 
 	private void enregistrerParametres(String urlDossierDesThemes, int nbMaxDeQuestionsParTheme, int nbMaxDeReponsesParQuestion) {
 		File paramsFile = new File("params.csv");
